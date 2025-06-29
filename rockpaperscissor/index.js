f34db22b3e5b32d4e94154573c9c3e7b4a38585b
Maaz Khan <63423339+Maaz-R-Khan@users.noreply.github.com>
2025-06-29 12:05:53 -0400

    added getUserChoice function and need to implement check win and add debugging statements

--- rockpaperscissor/index.js
+++ rockpaperscissor/index.js
@@ -6,35 +6,56 @@ function generateRandomChoice() {
 // Returns a random integer from 0 to 2:
 let randInt = Math.floor(Math.random() * 3); 
 
-let elements =  document.getElementsByClassName("result")
+
 
 //when you use the DOM method getElementsByClassName
 // a list of html elements with the className of result is going to be returned. 
-// so you need to define of which element's inner text you want to change. 
-
-
+// so you need to define which element's inner text you want to change. 
 
 if(randInt === 0) {
 console.log("The random integer is 0. ")
-for(let i = 0; i < elements.length; i++) {
- elements[i].innerText = choice1; 
-}
+document.getElementById("result").value = choice1; 
 
 }
 
 else if(randInt === 1) {
     console.log("The random integer is 1.")
-    for(let i = 0; i < elements.length; i++) {
-    elements[i].innerText = choice2; 
-}
+    document.getElementById("result").value = choice2; 
 }
 
 else {
     console.log("The random integer is 2.")
-   for(let i = 0; i < elements.length; i++) {
-    elements[i].innerText = choice3; 
-}
+  document.getElementById("result").value = choice3; 
 }
 
 }
 
+
+// Event listeners allow something to happen when a button is clicked. 
+// In this case, if a user clicks rock, then I want the button to 
+
+function getUserChoice() {
+ 
+
+  document.getElementById("rock-button").addEventListener("click", function outputRock() {
+   let userChoice = document.getElementById("user-output").value = choice1; 
+  }); 
+
+  document.getElementById("paper-button").addEventListener("click", function outputPaper() {
+    let userChoice = document.getElementById("user-output").value = choice2; 
+  })
+
+  document.getElementByID("scissor-button").addEventListener("click", function outputScissor() {
+    let userChoice = document.getElementById("user-output").value = choice3; 
+  })
+ 
+}
+
+
+
+
+function checkWin() {
+    if(userChoice === choice1 && compChoice == choice1) {
+        
+    }
+}
